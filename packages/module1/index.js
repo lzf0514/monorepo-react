034bebe842c3404c57f3563e4b98e0c32fbde22f
Lucas Lai <llai@xogrp.com>
2018-08-07 12:45:21 +0800

    yarn test

--- packages/module1/index.js
+++ packages/module1/index.js
@@ -1,3 +1,3 @@
 module.exports = function(){
-    return 'test module11';
+    return 'test module111';
 }
--- packages/module2/index.js
+++ packages/module2/index.js
@@ -1,3 +1,3 @@
 module.exports = function(){
-    return 'test module2';
+    return 'test module222';
 }
